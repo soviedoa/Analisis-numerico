cddbbee46b1ce3389fd8d9b4ede8615ae56e8ea2
soviedoa <69642082+soviedoa@users.noreply.github.com>
2022-06-02 01:39:00 -0500

    Update Cuadraticos.m

--- Cuadraticos.m
+++ Cuadraticos.m
@@ -1,4 +1,4 @@
-disp('MÉTODO: Vandermonde')
+disp('MÉTODO: trazadores cuadráticos')
 disp('Ingrese los puntos: ')
 x = input('Nota: cada que ingrese un punto, digite un punto y coma y encierrelos en corchetes. ');
 disp('Ingrese las imágenes de los puntos: ')
